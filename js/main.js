cf090819381fa2d2b1cd97e50d94f07808a852ce
Alan Ho <aho0593@gmail.com>
2023-04-10 23:36:24 +0000

    Listener added to form submit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 const $urlInput = document.querySelector('#img-url');
+const $img = document.querySelector('img');
 
 $urlInput.addEventListener('input', function (event) {
-  const $img = document.querySelector('img');
   $img.setAttribute('src', event.target.value);
+});
+
+const $form = document.querySelector('form');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const entry = {
+    title: $form.elements.title.value,
+    imgURL: $form.elements.url.value,
+    notes: $form.elements.notes.value,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId += 1;
+  data.entries.unshift(entry);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 });
