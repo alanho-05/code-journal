7f07c38f400bb18b18beb7f49f6f52cf89412b06
Alan Ho <aho0593@gmail.com>
2023-04-11 18:48:50 +0000

    Defined a function that return entries as DOM tree

--- js/main.js
+++ js/main.js
@@ -20,3 +20,39 @@ $form.addEventListener('submit', function (event) {
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 });
+
+function renderEntry(entry) {
+  const listEl = document.createElement('li');
+
+  const outerDiv = document.createElement('div');
+  outerDiv.setAttribute('class', 'row');
+
+  const imgDiv = document.createElement('div');
+  imgDiv.setAttribute('class', 'column-half');
+
+  const imgEl = document.createElement('img');
+  imgEl.setAttribute('src', entry.imgURL);
+  imgEl.setAttribute('alt', 'image of entry #' + entry.entryId);
+  imgEl.setAttribute('class', 'border-radius entry-img');
+
+  const txtDiv = document.createElement('div');
+  txtDiv.setAttribute('class', 'column-half');
+
+  const entryTitle = document.createElement('h3');
+  entryTitle.textContent = entry.title;
+
+  const entryTxt = document.createElement('p');
+  entryTxt.textContent = entry.notes;
+
+  listEl.appendChild(outerDiv);
+  outerDiv.appendChild(imgDiv);
+  imgDiv.appendChild(imgEl);
+  outerDiv.appendChild(txtDiv);
+  txtDiv.appendChild(entryTitle);
+  txtDiv.appendChild(entryTxt);
+
+  return listEl;
+}
+
+renderEntry(data.entries[0]);
+// Check image classes with entry-img class
